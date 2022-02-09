0d99aba6327a4fc077c02e91a4bd41e5e8c96812
woojin7879 <woojin7879@unist.ac.kr>
2022-02-09 22:05:32 +0900

    AxB

--- 12865.py
+++ 12865.py
@@ -13,3 +13,5 @@ for i in range(1,n+1):
         else:
             value[i][j] = max(value[i-1][j],value[i-1][j-array[i-1][0]]+array[i-1][1])
 print(value[-1][-1])
+
+#처음에 중복 안되는질 몰라서 좀 헤멨다. 질문 보다보니 중복 안되서 value를 이중으로 만들어 아이템 한개씩만 처리했다.

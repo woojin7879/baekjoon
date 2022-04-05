7d1958af0745b8d96425a4aa039cd2bc025f7aca
woojin7879 <woojin7879@unist.ac.kr>
2022-04-05 17:13:04 +0900

    2xn 타일링 2

---
+++ 11727.py
@@ -0,0 +1,9 @@
+#2xn 타일링 2
+import sys
+n = int(sys.stdin.readline())
+way = [0] * (n + 1)
+way[1] = 1
+way[2] = 3
+for i in range(3, n + 1):
+    way[i] = way[i - 1] + 2 * way[i - 2]
+print(way[-1] % 10007)

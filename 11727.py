1e61c37b1bea035fa0a27c7a610197ae81defd77
woojin7879 <woojin7879@unist.ac.kr>
2022-04-05 18:01:06 +0900

    2xn 타일링 2

--- 11727.py
+++ 11727.py
@@ -3,7 +3,8 @@ import sys
 n = int(sys.stdin.readline())
 way = [0] * (n + 1)
 way[1] = 1
-way[2] = 3
+if n > 1:
+    way[2] = 3
 for i in range(3, n + 1):
     way[i] = way[i - 1] + 2 * way[i - 2]
 print(way[-1] % 10007)
